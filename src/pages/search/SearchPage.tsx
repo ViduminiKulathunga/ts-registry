c77001d5d36239a0ff856aebe941bf11cade61ae
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-05-30 21:03:39 +0200

    connecting page and loader

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,12 +1,9 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
+import type { SearchLoaderResult } from "./searchLoader";
 
-export default function SearchPage(){
-    const data = useLoaderData();
+export default function SearchPage() {
+  const data = useLoaderData() as SearchLoaderResult;
 
-    console.log(data);
-    return(
-        <div>
-            Search
-        </div>
-    )
+  console.log(data);
+  return <div>Search</div>;
 }
