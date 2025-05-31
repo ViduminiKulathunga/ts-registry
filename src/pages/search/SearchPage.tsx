f0aedb9f0bfe18f577b1071db0cca5c234d948cd
ViduminiKulathunga <viduminikulathunga@gmail.com>
2025-05-31 10:06:27 +0200

    Packages list component added

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,9 +1,18 @@
 import { useLoaderData } from "react-router-dom";
 import type { SearchLoaderResult } from "./searchLoader";
+import PackageListItems from "../../components/PackageListItem";
 
 export default function SearchPage() {
-  const data = useLoaderData() as SearchLoaderResult;
+  const { searchResults } = useLoaderData() as SearchLoaderResult;
 
-  console.log(data);
-  return <div>Search</div>;
+  const renderResults = searchResults.map((result) => {
+    return <PackageListItems pack={result} key={result.name} />;
+  });
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold my-6">Search Results</h1>
+      <div className="space-y-4 mt-4">{renderResults}</div>
+    </div>
+  );
 }
